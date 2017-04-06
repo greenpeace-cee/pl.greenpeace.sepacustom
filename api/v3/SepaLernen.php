15f5e31021d30581bb7a580c3f296e555ce8fd38
systopia <endres@systopia.de>
2017-04-06 23:25:38 +0200

    reworked sepa_lernen

--- api/v3/SepaLernen.php
+++ api/v3/SepaLernen.php
@@ -31,37 +31,51 @@ function civicrm_api3_sepa_lernen_find($params) {
     return civicrm_api3_create_error("Table 'sepa_lernen' not found.");
   }
 
+  // this is an a field array. The outer one is AND, the inner one OR,
+  //  e.g. (('f1', 'f2'),('f3', 'f4')) results in
+  //  SELECT ... FROM ... WHERE ((f1=@val1 OR f2=@val2) AND (f3=@val3 OR f4=@val4))
+  $fields = array(
+    array('debitor_name', 'debitor_iban'),
+    array('remote_information_usage', 'remote_information_reference', 'endtoendid'),
+    array('spendenbetrag'),
+  );
+
   // compile query
-  $fields = array('debitor_name', 'remote_information_usage', 'remote_information_reference', 'debitor_iban', 'spendenbetrag');
-  $query_clauses = array();
   $query_parameters = array();
   $counter = 1;
-  foreach ($fields as $field) {
-    if (!empty($params[$field])) {
-      $query_clauses[] = "`{$field}` LIKE %{$counter}";
-      $query_parameters[$counter] = array($params[$field], 'String');
-      $counter += 1;
+  $ANDclauses = array();
+  foreach ($fields as $field_list) {
+    $ORclauses = array();
+    foreach ($field_list as $field) {
+      if (!empty($params[$field])) {
+        $query_parameters[$counter] = array($params[$field], 'String');
+        $ORclauses[] = "`{$field}` LIKE %{$counter}";
+        $counter += 1;
+      }
+    }
+    if (!empty($ORclauses)) {
+      // at least one of the fields of the clause is present
+      $ANDclauses[] = '(' . implode(') OR (', $ORclauses) .')';
     }
   }
 
-  if (empty($query_clauses)) {
-    return civicrm_api3_create_error("No query parameters given.");
-  }
+  if (!empty($ANDclauses)) {
+    $where_clause = '(' . implode(') AND (', $ANDclauses) . ')';
+    $query = CRM_Core_DAO::executeQuery("SELECT kntsepalearnid, aktionid, personid, vertragid FROM sepa_lernen WHERE {$where_clause} AND status = 1;", $query_parameters);
+    $results = array();
+    while ($query->fetch()) {
+      $results[$query->kntsepalearnid] = array(
+        'id' => $query->kntsepalearnid,
+        'aktionid'  => $query->aktionid,
+        'personid'  => $query->personid,
+        'vertragid' => $query->vertragid,
+        'reference' => "{$query->aktionid}_{$query->personid}_{$query->vertragid}");
+    }
 
-  // execute query
-  $where_clause = implode(' AND ', $query_clauses);
-  $query = CRM_Core_DAO::executeQuery("SELECT kntsepalearnid, aktionid, personid, vertragid FROM sepa_lernen WHERE {$where_clause} AND status = 1;", $query_parameters);
-  $results = array();
-  while ($query->fetch()) {
-    $results[$query->kntsepalearnid] = array(
-      'id' => $query->kntsepalearnid,
-      'aktionid'  => $query->aktionid,
-      'personid'  => $query->personid,
-      'vertragid' => $query->vertragid,
-      'reference' => "{$query->aktionid}_{$query->personid}_{$query->vertragid}");
+    return civicrm_api3_create_success($results);
+  } else {
+    return civicrm_api3_create_error("Not enough values");
   }
-
-  return civicrm_api3_create_success($results);
 }
 
 /**
@@ -72,6 +86,10 @@ function _civicrm_api3_sepa_lernen_find_spec(&$params) {
     'title' => 'Debitor Name',
     'type' => CRM_Utils_Type::T_STRING,
   );
+  $params['debitor_iban'] = array(
+    'title' => "Debitor's IBAN",
+    'type' => CRM_Utils_Type::T_STRING,
+  );
   $params['remote_information_usage'] = array(
     'title' => 'Assignment',
     'type' => CRM_Utils_Type::T_STRING,
@@ -80,8 +98,8 @@ function _civicrm_api3_sepa_lernen_find_spec(&$params) {
     'title' => 'Reference',
     'type' => CRM_Utils_Type::T_STRING,
   );
-  $params['debitor_iban'] = array(
-    'title' => "Debitor's IBAN",
+  $params['endtoendid'] = array(
+    'title' => 'EndToEndId',
     'type' => CRM_Utils_Type::T_STRING,
   );
   $params['debitor_spendenbetrag'] = array(
