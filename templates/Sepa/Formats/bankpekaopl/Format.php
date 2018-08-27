c8a328a93316161a07a863890f4ca80913ae2669
Patrick Figel <pfigel@greenpeace.org>
2018-08-27 13:50:32 +0200

    Set display_name with original encoding
    CiviSEPA converts display_name to ASCII, but we need the original
    name.

--- templates/Sepa/Formats/bankpekaopl/Format.php
+++ templates/Sepa/Formats/bankpekaopl/Format.php
@@ -38,8 +38,8 @@ class CRM_Sepa_Logic_Format_bankpekaopl extends CRM_Sepa_Logic_Format {
       'donation_description' => 'Darowizna na cele',
       'creditor_name_prefix' => 'Fundacja',
       'statutory_foundation' => 'statutowe Fundacji',
-	  'encoding_input' => 'UTF-8',
-	  'encoding_output' => 'CP852',
+      'encoding_input' => 'UTF-8',
+      'encoding_output' => 'CP852',
     ));
   }
   
@@ -49,12 +49,17 @@ class CRM_Sepa_Logic_Format_bankpekaopl extends CRM_Sepa_Logic_Format {
    *  transaction (contribution + extra data)
    */
   public function extendTransaction(&$txn, $creditor_id) {
-	$contribution_recur_id = $txn["contribution_recur_id"];
+    $contribution_recur_id = $txn["contribution_recur_id"];
     $result = CRM_Core_DAO::executeQuery("select m.entity_id as membership_id from civicrm_value_membership_payment m where m.membership_recurring_contribution = " . $contribution_recur_id);
-	while ($result->fetch()){	
-		$txn['membership_id'] = $result->membership_id;	
-	}
-	return $txn;
+    while ($result->fetch()){
+      $txn['membership_id'] = $result->membership_id;
+    }
+    // set display_name in original encoding
+    $result = CRM_Core_DAO::executeQuery("select display_name from civicrm_contact where id = %0", [[$txn['contact_id'], 'Integer']]);
+    while ($result->fetch()) {
+      $txn['display_name'] = $result->display_name;
+    }
+    return $txn;
   }
   
 
