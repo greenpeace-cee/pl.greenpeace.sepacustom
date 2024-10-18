40a5220df87dfc411b626d484638ccf0a231decb
Patrick Figel <pfigel@greenpeace.org>
2024-10-18 10:52:38 +0200

    GP-43955 Upgrade civix

--- sepacustom.php
+++ sepacustom.php
@@ -143,7 +143,6 @@ function sepacustom_civicrm_modify_txgroup_reference(&$reference, $creditor_id, 
   }
 }
 
-
 /**
  * Connect newly generated SEPA contributions
  *  to the membership/contract
@@ -178,7 +177,6 @@ function sepacustom_civicrm_modify_txmessage(&$txmessage, $info, $creditor) {
   $txmessage = "Greenpeace Beitrag Danke - {$info['reference']}";
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -186,17 +184,6 @@ function sepacustom_civicrm_modify_txmessage(&$txmessage, $info, $creditor) {
  */
 function sepacustom_civicrm_config(&$config) {
   _sepacustom_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function sepacustom_civicrm_xmlMenu(&$files) {
-  _sepacustom_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -209,15 +196,6 @@ function sepacustom_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function sepacustom_civicrm_uninstall() {
-  _sepacustom_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
@@ -227,79 +205,4 @@ function sepacustom_civicrm_enable() {
 
   $customData = new CRM_Sepacustom_CustomData('pl.greenpeace.sepacustom');
   $customData->syncOptionGroup(__DIR__ . '/resources/formats_option_group.json');
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function sepacustom_civicrm_disable() {
-  _sepacustom_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function sepacustom_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _sepacustom_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function sepacustom_civicrm_managed(&$entities) {
-  _sepacustom_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function sepacustom_civicrm_caseTypes(&$caseTypes) {
-  _sepacustom_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function sepacustom_civicrm_angularModules(&$angularModules) {
-_sepacustom_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function sepacustom_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _sepacustom_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
